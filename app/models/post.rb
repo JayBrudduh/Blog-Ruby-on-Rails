3e702d51fd256114629611ff02aa46af79f75bfd
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-05-11 16:55:20 -0400

    Add after save callback to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_save :increment_counter
+
   def last_five_comments
     comments.order(created_at: :desc).limit(5)
   end
 
+  private
   def increment_counter
     author.increment!(:post_counter)
   end
