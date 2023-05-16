626362dd53dd53414b3f2d9e31a3c9ca462489b9
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-05-16 18:05:17 -0400

    No linter errors 2

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Post', type: :request do
   describe 'GET /posts' do
-
     it 'return http success' do
       get '/users/1/posts'
       expect(response).to have_http_status(200)
@@ -17,15 +16,12 @@ RSpec.describe 'Post', type: :request do
       get '/users/1/posts/show'
       expect(response).to render_template 'show'
     end
-
   end
 end
 
 RSpec.describe 'Response body includes correct placeholder text:', type: :feature do
-
   scenario 'should content User Post Home text' do
     visit '/users/1/posts'
     expect(page).to have_content('User Post Home')
   end
-
 end
