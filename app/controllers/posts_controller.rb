2cc711c0b3f9a9679cfaadcb787f3c6575f339b5
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-05-18 15:02:35 -0400

    No linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
   end
+
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
