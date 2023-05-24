bc8aaa3c6e67dd63f4e8cbb8dc7ecbb83338f884
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-05-24 14:17:24 -0400

    No linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password, :post_counter, :photo)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :bio, :email, :password, :current_password, :photo)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password, :post_counter, :photo) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name,
+                                                                     :bio,
+                                                                     :email,
+                                                                     :password,
+                                                                     :current_password,
+                                                                     :photo) }
   end
 end
