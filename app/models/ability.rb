28ee571025be882497e6af25fa0fc59bbe4a2276
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-05-25 16:17:13 -0400

    No linter errors 2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -9,7 +7,7 @@ class Ability
     if user.role == 'admin'
       can :manage, :all
     elsif user.role == 'user'
-      can [:read, :create], :all
+      can %i[read, create], :all
       can :destroy, Post do |post|
         puts post.author
         puts user
